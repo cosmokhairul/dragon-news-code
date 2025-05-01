651af9c69453d355c474580c7857c61192a7234b
cosmokhairul <porosh.means.touch@gmail.com>
2025-05-01 21:21:28 +0600

    Header Added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,15 @@
 import React from "react";
+import logo from "../assets/logo.png";
+import { format } from "date-fns";
 
 const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
-      <h2>Gellp </h2>
-      <h2>Gellp </h2>
-      <h2>Gellp </h2>
-      <h2>Gellp </h2>
-      <h2>Gellp </h2>
-      <h2>Gellp </h2>
+    <div className="flex justify-center flex-col items-center gap-3">
+      <img className="w-[400px]" src={logo} alt="" />
+      <p className="text-accent">Journalism without Fear or Favour</p>
+      <p className="font-semibold text-accent">
+        {format(new Date(), "EEEE, MMMM MM, yyyy")}
+      </p>
     </div>
   );
 };
