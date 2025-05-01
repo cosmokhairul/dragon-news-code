e901257ed01ec0e0f49127bde91fce9606d8d6b0
cosmokhairul <porosh.means.touch@gmail.com>
2025-05-02 00:44:42 +0600

    React fast marque added

--- src/components/LatestNews.jsx
+++ src/components/LatestNews.jsx
@@ -1,14 +1,27 @@
 import React from "react";
+import Marquee from "react-fast-marquee";
 
 const LatestNews = () => {
   return (
     <div className="flex items-center gap-5 bg-base-200 p-3 ">
       <p className="text-base-100 bg-secondary px-3 py-2">Latest</p>
-      <p className="font-bold">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas
-        consequatur totam illo laudantium ea repellat, error quod quae vel sed
-        ex eveniet dolores fugiat sit similique consequuntur est minima atque.
-      </p>
+      <Marquee className="flex gap-10" pauseOnHover={true} speed={100}>
+        <p className="font-bold">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas
+          consequatur totam illo laudantium ea repellat, error quod quae vel sed
+          ex eveniet dolores fugiat sit similique consequuntur est minima atque.
+        </p>
+        <p className="font-bold">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas
+          consequatur totam illo laudantium ea repellat, error quod quae vel sed
+          ex eveniet dolores fugiat sit similique consequuntur est minima atque.
+        </p>
+        <p className="font-bold">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas
+          consequatur totam illo laudantium ea repellat, error quod quae vel sed
+          ex eveniet dolores fugiat sit similique consequuntur est minima atque.
+        </p>
+      </Marquee>
     </div>
   );
 };
