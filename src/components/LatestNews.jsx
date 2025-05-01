51f0242211875e1bdd3e0d8a246df0d6470b0c68
cosmokhairul <porosh.means.touch@gmail.com>
2025-05-01 23:18:35 +0600

    Latest News section added

---
+++ src/components/LatestNews.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const LatestNews = () => {
+  return (
+    <div className="flex items-center gap-5 bg-base-200 p-3 ">
+      <p className="text-base-100 bg-secondary px-3 py-2">Latest</p>
+      <p className="font-bold">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas
+        consequatur totam illo laudantium ea repellat, error quod quae vel sed
+        ex eveniet dolores fugiat sit similique consequuntur est minima atque.
+      </p>
+    </div>
+  );
+};
+
+export default LatestNews;
