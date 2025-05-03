58cbbc4a2d6020fe4d1de4a2464e94319be3b3e5
cosmokhairul <porosh.means.touch@gmail.com>
2025-05-03 22:42:12 +0600

    Login Page designed and added

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import { Outlet } from "react-router";
 
 const AuthLayout = () => {
   return (
-    <div>
+    <div className="bg-base-200 min-h-screen">
       <header className="w-11/12 mx-auto py-4">
         <Navbar></Navbar>
       </header>
+
+      <main className="w-11/12 mx-auto py-5">
+        <Outlet></Outlet>
+      </main>
     </div>
   );
 };
