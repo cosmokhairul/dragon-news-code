65d386f480da7bde46dbe4e5ef4eecce35d6f4dc
cosmokhairul <porosh.means.touch@gmail.com>
2025-05-03 23:50:36 +0600

    Register Page designed and added

--- src/components/homelayout/Qzone.jsx
+++ src/components/homelayout/Qzone.jsx
@@ -5,10 +5,10 @@ import playImage from "../../assets/playground.png";
 
 const Qzone = () => {
   return (
-    <div className="bg-base-200 p-3">
+    <div className="bg-base-200 p-3 rounded-lg">
       <h1 className="font-bold mb-5">QZone</h1>
 
-      <div className="space-y-5">
+      <div className="space-y-5 flex flex-col justify-center p-10">
         <img src={swimmingImage} alt="" />
         <img src={classImage} alt="" />
         <img src={playImage} alt="" />
