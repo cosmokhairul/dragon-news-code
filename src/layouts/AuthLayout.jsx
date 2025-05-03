bfafccd78870daf06a6e37f01fab4b0e615ffd58
cosmokhairul <porosh.means.touch@gmail.com>
2025-05-03 22:07:43 +0600

    Auth Layout Added

---
+++ src/layouts/AuthLayout.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+const AuthLayout = () => {
+  return (
+    <div>
+      <header className="w-11/12 mx-auto py-4">
+        <Navbar></Navbar>
+      </header>
+    </div>
+  );
+};
+
+export default AuthLayout;
