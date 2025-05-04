80fc5f588c1a5ca8059d924faaa2d2eed2d211a6
cosmokhairul <porosh.means.touch@gmail.com>
2025-05-04 22:38:48 +0600

    Firebase Installed

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBA3vD4BKnrfrsmiiN4ujN2qmb1BjPX4Tc",
+  authDomain: "dragon-news-2c9c9.firebaseapp.com",
+  projectId: "dragon-news-2c9c9",
+  storageBucket: "dragon-news-2c9c9.firebasestorage.app",
+  messagingSenderId: "769688314197",
+  appId: "1:769688314197:web:4da0d3f6ef37b05ae3cfdc",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,28 +1,75 @@
-import React from "react";
+import React, { use } from "react";
 import { Link } from "react-router";
+import { AuthContext } from "../provider/AuthProvider";
 
 const Register = () => {
+  const { createUser, setUser } = use(AuthContext);
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+    const form = e.target;
+    const name = form.name.value;
+    const photo = form.photo.value;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log({ name, photo, email, password });
+    createUser(email, password)
+      .then((result) => {
+        const user = result.user;
+        setUser(user);
+        // console.log(user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert(errorCode, errorMessage);
+      });
+  };
+
   return (
     <div className="flex justify-center min-h-screen items-center">
       <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl py-5">
         <h2 className="text-2xl font-semibold text-center">
           Register Your Account
         </h2>
-        <div className="card-body">
+        <form onSubmit={handleRegister} className="card-body">
           <fieldset className="fieldset">
             <label className="label">Name</label>
-            <input type="text" className="input" placeholder="Name" />
+            <input
+              name="name"
+              type="text"
+              className="input"
+              placeholder="Name"
+            />
 
             <label className="label">Photo URL</label>
-            <input type="text" className="input" placeholder="Photo URL" />
+            <input
+              name="photo"
+              type="text"
+              className="input"
+              placeholder="Photo URL"
+            />
 
             <label className="label">Email</label>
-            <input type="email" className="input" placeholder="Email" />
+            <input
+              name="email"
+              type="email"
+              className="input"
+              placeholder="Email"
+            />
 
             <label className="label">Password</label>
-            <input type="password" className="input" placeholder="Password" />
+            <input
+              name="password"
+              type="password"
+              className="input"
+              placeholder="Password"
+            />
 
-            <button className="btn btn-neutral mt-4">Register</button>
+            <button type="submit" className="btn btn-neutral mt-4">
+              Register
+            </button>
 
             <p className="font-semibold text-center pt-5">
               Already have an account?{" "}
@@ -31,7 +78,7 @@ const Register = () => {
               </Link>
             </p>
           </fieldset>
-        </div>
+        </form>
       </div>
     </div>
   );
