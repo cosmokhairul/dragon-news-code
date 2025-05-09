4a5d7164c44f5a475404e39c7d6d78eb11d2810c
cosmokhairul <porosh.means.touch@gmail.com>
2025-05-09 23:25:45 +0600

    Styles added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import { format } from "date-fns";
 
 const Header = () => {
   return (
-    <div className="flex justify-center flex-col items-center gap-3">
+    <div className="flex justify-center flex-col items-center gap-3 py-5">
       <img className="w-[400px]" src={logo} alt="" />
       <p className="text-accent">Journalism without Fear or Favour</p>
       <p className="font-semibold text-accent">
