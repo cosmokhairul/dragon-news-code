4a9c734c1b8cee6a394688476a5feef21fc4720c
cosmokhairul <porosh.means.touch@gmail.com>
2025-05-17 23:46:26 +0600

    styles added

--- src/components/LatestNews.jsx
+++ src/components/LatestNews.jsx
@@ -6,17 +6,17 @@ const LatestNews = () => {
     <div className="flex items-center gap-5 bg-base-200 p-3 ">
       <p className="text-base-100 bg-secondary px-3 py-2">Latest</p>
       <Marquee className="flex gap-10" pauseOnHover={true} speed={100}>
-        <p className="font-bold">
+        <p className="font-semibold">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas
           consequatur totam illo laudantium ea repellat, error quod quae vel sed
           ex eveniet dolores fugiat sit similique consequuntur est minima atque.
         </p>
-        <p className="font-bold">
+        <p className="font-semibold">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas
           consequatur totam illo laudantium ea repellat, error quod quae vel sed
           ex eveniet dolores fugiat sit similique consequuntur est minima atque.
         </p>
-        <p className="font-bold">
+        <p className="font-semibold">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas
           consequatur totam illo laudantium ea repellat, error quod quae vel sed
           ex eveniet dolores fugiat sit similique consequuntur est minima atque.
--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -68,7 +68,7 @@ const NewsCard = ({ news }) => {
       </div>
 
       {/* Footer */}
-      <div className="flex justify-between items-center mt-4 border-t border-gray-300 p-5">
+      <div className="flex justify-between items-center mt-5 border-t border-gray-300 p-6">
         <div className="flex items-center gap-1 text-orange-500">
           <FaStar className="text-lg" />
           <span className="font-semibold text-gray-800">{rating.number}</span>
